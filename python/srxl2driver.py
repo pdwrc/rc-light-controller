2bf16d0bc74195b84c6c43db1e6f78dc1dfd339e
Paul Warren <pdw@ex-parrot.com>
2022-06-07 12:05:21 +0100

    Process only the most recent packet.

--- python/srxl2driver.py
+++ python/srxl2driver.py
@@ -14,16 +14,28 @@ class SRXL2Driver:
         self.srxl2 = SRXL2()
 
     def process(self):
+
+        telemetry_packet = None
+        control_packet = None
+
         while self.u.any() > 0:
             self.rx += self.u.read()
             if len(self.rx) > 2 and len(self.rx) >= self.rx[2]:
                 packet = self.srxl2.parse(self.rx)
                 if type(packet) == SRXL2Control:
-                    self.control_callback(packet.pwm_channel_data)
+                    control_packet = packet
                 elif type(packet) == SRXL2Telemetry and packet.is_esc_telemetry:
-                    self.telemetry_callback(packet)
+                    telemetry_packet = packet
                 self.rx = self.rx[self.rx[2]:]
             self.lastt = time.ticks_us()
+
+        if telemetry_packet is not None or control_packet is not None:
+            self.rx = bytes()
+        if control_packet is not None:
+            self.control_callback(control_packet.pwm_channel_data)
+        if telemetry_packet is not None:
+            self.telemetry_callback(telemetry_packet)
+
         # Throw away any remaining bytes after 100us of inactivity
         if time.ticks_us() - self.lastt > 100:
             self.rx = bytes()
